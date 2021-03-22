7f5280af29b962d638257300f5293f50ad8be2c7
Rebecca Mead <beckyleemead@gmail.com>
2021-03-22 11:04:15 -0400

    test submitSingleMessage

--- data_exchanger.php
+++ data_exchanger.php
@@ -1,13 +1,13 @@
 <?php
 class Data_Exchanger
 {
-  // KYIR db credentials - pass in to new instance
+  // data to send to KYIR - pass in to new instance
   public $kyir_username;
   public $kyir_password;
   public $kyir_facility_id;
   public $hl7_message;
 
-  function __construct($kyir_username, $kyir_password, $kyir_facility_id)
+  function __construct($kyir_username, $kyir_password, $kyir_facility_id, $hl7_message)
   {
     $this->kyir_username = $kyir_username;
     $this->kyir_password = $kyir_password;
@@ -15,8 +15,8 @@ class Data_Exchanger
     $this->hl7_message = $hl7_message;
   }
 
-  // connect to KYIR - called by other functions
-  function call_kyir($hl7_message)
+  // function to send a message to KYIR and receive the result
+  function call_kyir()
   {
     $wsdl = "https://kyirqa.chfs.ky.gov/HL7Engine_QA/Cdc.aspx?WSDL";
     $kyir_server = "https://kyirqa.chfs.ky.gov/HL7Engine_QA/cdc/v1/iisservice.svc";
@@ -27,15 +27,30 @@ class Data_Exchanger
         'location' => $kyir_server
       )
     );
+
+    // testing: get functions from WSDL
     print_r($kyir_soap_client->__getFunctions());
 
     echo ("<br><br>");
 
+    // testing: echoBack
     $connectivity_test_params = array(
       'echoBack' => 'hellooooo'
     );
-    $result = $kyir_soap_client->connectivityTest($connectivity_test_params);
-    var_dump($result);
+    $connectivity_result = $kyir_soap_client->connectivityTest($connectivity_test_params);
+    var_dump($connectivity_result);
+
+    echo ("<br><br>");
+
+    // testing: submitSingleMessage
+    $submit_single_message_params = array(
+      'username' => $kyir_username,
+      'password' => $kyir_password,
+      'facilityID' => $kyir_facility_id,
+      'hl7Message' => $hl7_message
+    );
+    $message_result = $kyir_soap_client->connectivityTest($submit_single_message_params);
+    var_dump($message_result);
 
     // return: connection status / error
   }
@@ -44,5 +59,5 @@ class Data_Exchanger
 
 }
 
-$test_run = new Data_Exchanger('username', 'password', 'facility_id');
-print_r($test_run->call_kyir('message'));
+$test_run = new Data_Exchanger('username', 'password', 'facility_id', 'hl7_message');
+print_r($test_run->call_kyir());

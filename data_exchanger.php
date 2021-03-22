a0085ee05849f8c45376a29a46d2cca8df4ea304
Rebecca Mead <beckyleemead@gmail.com>
2021-03-22 13:09:54 -0400

    clean up extraneous testing and pseudocode

--- data_exchanger.php
+++ data_exchanger.php
@@ -4,6 +4,7 @@ class Data_Exchanger
   // function to send a message to KYIR and receive the result
   function call_kyir($kyir_username, $kyir_password, $kyir_facility_id, $hl7_message)
   {
+    // set up soap client
     $wsdl = "https://kyirqa.chfs.ky.gov/HL7Engine_QA/Cdc.aspx?WSDL";
     $kyir_server = "https://kyirqa.chfs.ky.gov/HL7Engine_QA/cdc/v1/iisservice.svc";
     $kyir_soap_client = new SoapClient(
@@ -14,33 +15,16 @@ class Data_Exchanger
       )
     );
 
-    // testing: get functions from WSDL
-    print_r($kyir_soap_client->__getFunctions());
-
-    echo ("<br><br>");
-
-    // testing: echoBack
-    $connectivity_test_params = array(
-      'echoBack' => 'hellooooo'
-    );
-    $connectivity_result = $kyir_soap_client->connectivityTest($connectivity_test_params);
-    var_dump($connectivity_result);
-
-    echo ("<br><br>");
-
-    // testing: submitSingleMessage
+    // define parameters for submitSingleMessage
     $submit_single_message_params = array(
       'username' => $kyir_username,
       'password' => $kyir_password,
       'facilityID' => $kyir_facility_id,
       'hl7Message' => $hl7_message
     );
-    $message_result = $kyir_soap_client->submitSingleMessage($submit_single_message_params);
-    var_dump($message_result);
 
-    // return: connection status / error
+    // send hl7 message to KYIR server and return result
+    return $kyir_soap_client->submitSingleMessage($submit_single_message_params);
   }
-
-  // close client/connection?
 
 }

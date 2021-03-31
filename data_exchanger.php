587084af558cee64ae3b4785671e2f2d943976fa
Rebecca Mead <beckyleemead@gmail.com>
2021-03-31 13:12:44 -0400

    add initial structure for new function generate_hl7_message

--- data_exchanger.php
+++ data_exchanger.php
@@ -1,16 +1,24 @@
 <?php
 class Data_Exchanger
 {
-  // function to send a message to KYIR and receive the result
-  function call_kyir($kyir_username, $kyir_password, $kyir_facility_id, $hl7_message)
+  // function to properly format data from LFCHD database into HL7 message
+  public function generate_hl7_message($input_data)
   {
+    return $input_data;
+  }
+
+  // function to send a message to KYIR and receive the result
+  public function call_kyir($kyir_username, $kyir_password, $kyir_facility_id, $input_data)
+  {
+    $hl7_message = $this->generate_hl7_message($input_data);
+
     // set up soap client
     $wsdl = "https://kyirqa.chfs.ky.gov/HL7Engine_QA/Cdc.aspx?WSDL";
     $kyir_server = "https://kyirqa.chfs.ky.gov/HL7Engine_QA/cdc/v1/iisservice.svc";
     $kyir_soap_client = new SoapClient(
       $wsdl,
       array(
-        'soap_version' => SOAP_1_2, // need this to read content correctly
+        'soap_version' => SOAP_1_2,
         'location' => $kyir_server
       )
     );

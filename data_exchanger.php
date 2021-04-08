28cf69c99803f3d6df865297ad8468afc9af7edd
Rebecca Mead <beckyleemead@gmail.com>
2021-04-08 10:31:33 -0400

    abstract more variables from hl7 message for further generalization

--- data_exchanger.php
+++ data_exchanger.php
@@ -5,19 +5,37 @@ class Data_Exchanger
   public function generate_hl7_message($input_data)
   {
     // define variables for hl7 message
-    // hard-coded variables
-    $sending_facility = 'LFUCGOVERNMENT';
-    $address_state = 'KY';
-
+    // hard-coded variables that can remain hard-coded
+    $sending_facility = "LFUCGOVERNMENT";
+    $receiving_application = "KY0000";
+    $receiving_facility = "KY0000";
+    $identifier_type = "SS";
+    $address_state = "KY";
+    $vaccine_code = "212^Janssen COVID-19 Vaccine^CVX^59676-0580-05^SARS-COV-2 (COVID-19) vaccine, vector non-replicating, recombinant spike protein-Ad26, preservative free, 0.5 mL^NDC";
+    $vaccine_amount = "0.5";
+    $vaccine_units = "mL^^UCUM";
+    $administration_notes = "00^NEW IMMUNIZATION RECORD^NIP001";
+    $vaccine_manufacturer = "JSN^Janssen Products, LP^MVX";
+    $administration_route = "C28161^IM^NCIT^IM^^HL70162";
+    // hard-coded variables that cannot remain hard-coded (currently fake data)
+    $vis_publish_date = "20210115";
+    $provider_last_name = "Olamina";
+    $provider_first_name = "Lauren";
+    $administered_location = "LFUCGOVERNMENT";
+    $vaccine_lot_number = "123456";
+    $vaccine_expiration_date = "20210620";
+    $administration_site = "LD^Left Deltoid^HL70163";
+    $observation_method = "VXC40^Eligibility captured at the immunization level^CDCPHINVS";
+    $vaccine_funding_source = "VXC50^Public^CDCPHINVS";
     // info from $input_data (many require reformatting)
-    $input_data_array = explode(',', $input_data);
+    $input_data_array = explode(",", $input_data);
     $pef_id = $input_data_array[0];
-    $first_name = $input_data_array[1];
-    $last_name = $input_data_array[2];
+    $patient_first_name = $input_data_array[1];
+    $patient_last_name = $input_data_array[2];
     $address_street = $input_data_array[3];
     $address_city = $input_data_array[4];
     $address_zip = $input_data_array[5];
-    $dob_unformatted = explode('/', $input_data_array[6]);
+    $dob_unformatted = explode("/", $input_data_array[6]);
     $dob = $dob_unformatted[2] . str_pad($dob_unformatted[0], 2, "0", STR_PAD_LEFT) . str_pad($dob_unformatted[1], 2, "0", STR_PAD_LEFT);
     $phone_number = substr($input_data_array[7], 0, 3) . "^" . substr($input_data_array[7], 3);
     $race = "";
@@ -61,25 +79,27 @@ class Data_Exchanger
         $sex = "";
         break;
     };
+    // tbd: which of the following three do we use for PID-3?
+    // update $identifier_type to match
     $medicaid_id = $input_data_array[16];
     $medicare_id = $input_data_array[17];
     $ssn = $input_data_array[18];
     $check_in_date_split = substr($input_data_array[19], 0, strpos($input_data_array[19], " "));
-    $check_in_date_unformatted = explode('/', $check_in_date_split);
+    $check_in_date_unformatted = explode("/", $check_in_date_split);
     $check_in_date = $check_in_date_unformatted[2] . str_pad($check_in_date_unformatted[0], 2, "0", STR_PAD_LEFT) . str_pad($check_in_date_unformatted[1], 2, "0", STR_PAD_LEFT);
 
     // automatically-generated variables
-    $time_current = date('YmdHis');
-    $time_incrementing = date('ymd') . 'KY000001';
+    $time_current = date("YmdHis");
+    $time_incrementing = date("ymd") . "KY000001";
 
-    $hl7_message = "MSH|^~\&||$sending_facility|KY0000|KY0000|$time_current||VXU^V04^VXU_V04|$time_incrementing|T|2.5.1|||||||||
-    PID|1||012345^^^$sending_facility^MR||$last_name^$first_name^^^^^L||$dob|$sex||$race|$address_street^^$address_city^$address_state^$address_zip^USA^L||^PRN^PH^^^$phone_number|||||||||$ethnic_group
+    $hl7_message = "MSH|^~\&||$sending_facility|$receiving_application|$receiving_facility|$time_current||VXU^V04^VXU_V04|$time_incrementing|T|2.5.1|||||||||
+    PID|1||$ssn^^^$sending_facility^$identifier_type||$patient_last_name^$patient_first_name^^^^^L||$dob|$sex||$race|$address_street^^$address_city^$address_state^$address_zip^USA^L||^PRN^PH^^^$phone_number|||||||||$ethnic_group
     ORC|RE||$pef_id|||||||||
-    RXA|0|1|$check_in_date||212^Janssen COVID-19 Vaccine^CVX^59676-0580-05^SARS-COV-2 (COVID-19) vaccine, vector non-replicating, recombinant spike protein-Ad26, preservative free, 0.5 mL^NDC|0.5|mL^^UCUM||00^NEW IMMUNIZATION RECORD^NIP001|^OLAMINA^LAUREN|^^^$sending_facility||||123456|20210620|JSN^Janssen Products, LP^MVX|||CP|A
-    RXR|C28161^IM^NCIT^IM^^HL70162|LD^Left Deltoid^HL70163
-    OBX|1|CE|64994-7^Vaccine fund pgm elig cat^LN|1|V01^Not VFC Eligible^HL70064||||||F||||||VXC40^Eligibility captured at the immunization level^CDCPHINVS
-    OBX|2|CE|30963-3^Vaccine funding source^LN|1|VXC50^Public^CDCPHINVS||||||F|||
-    OBX|3|CE|29768-9^Date vaccine information statement published^LN|1|20210115||||||F
+    RXA|0|1|$check_in_date||$vaccine_code|$vaccine_amount|$vaccine_units||$administration_notes|^$provider_last_name^$provider_first_name|^^^$administered_location||||$vaccine_lot_number|$vaccine_expiration_date|$vaccine_manufacturer|||CP|A
+    RXR|$administration_route|$administration_site
+    OBX|1|CE|64994-7^Vaccine fund pgm elig cat^LN|1|V01^Not VFC Eligible^HL70064||||||F||||||$observation_method
+    OBX|2|CE|30963-3^Vaccine funding source^LN|1|$vaccine_funding_source||||||F|||
+    OBX|3|CE|29768-9^Date vaccine information statement published^LN|1|$vis_publish_date||||||F
     OBX|4|CE|29769-7^Date vaccine information statement presented^LN|1|$check_in_date||||||F";
 
     return $hl7_message;
@@ -96,17 +116,17 @@ class Data_Exchanger
     $kyir_soap_client = new SoapClient(
       $wsdl,
       array(
-        'soap_version' => SOAP_1_2,
-        'location' => $kyir_server
+        "soap_version" => SOAP_1_2,
+        "location" => $kyir_server
       )
     );
 
     // define parameters for submitSingleMessage
     $submit_single_message_params = array(
-      'username' => $kyir_username,
-      'password' => $kyir_password,
-      'facilityID' => $kyir_facility_id,
-      'hl7Message' => $hl7_message
+      "username" => $kyir_username,
+      "password" => $kyir_password,
+      "facilityID" => $kyir_facility_id,
+      "hl7Message" => $hl7_message
     );
 
     // send hl7 message to KYIR server and return result
